fad48ee31c9d705e3dbbaf231fd695c7fc02452e
Ashwinik63-boop <124232914+Ashwinik63-boop@users.noreply.github.com>
2024-09-17 09:30:24 +0530

    Update fraud detection.py

--- fraud detection.py
+++ fraud detection.py
@@ -1,10 +1,7 @@
-
 # import the required packages
 
 import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
-import seaborn as sns
 import sys
 import scipy
 import sklearn
@@ -13,36 +10,22 @@ import sklearn
 data = pd.read_csv(r"C:\Users\ashwi\OneDrive\Desktop\creditcard.csv")
 
 # dataset exploring
-print(data.columns)
+
 
 # Print the shape of the data
-data = data.sample(frac=0.1, random_state = 1)
+data = data.sample(frac=0.1, random_state=1)
 print(data.shape)
-print(data.describe())
 
-# V1 - V28 are the results of a PCA Dimensionality reduction to protect user identities and sensitive features
-
-# Plot histograms of each parameter 
-data.hist(figsize = (20, 20))
-plt.show()
 
 # Determine number of fraud cases in dataset
-
 Fraud = data[data['Class'] == 1]
 Valid = data[data['Class'] == 0]
 
-outlier_fraction = len(Fraud)/float(len(Valid))
+outlier_fraction = len(Fraud) / float(len(Valid))
 print(outlier_fraction)
 
 print('Fraud Cases: {}'.format(len(data[data['Class'] == 1])))
 print('Valid Transactions: {}'.format(len(data[data['Class'] == 0])))
-
-# Correlation matrix
-corrmat = data.corr()
-fig = plt.figure(figsize = (12, 9))
-
-sns.heatmap(corrmat, vmax = .8, square = True)
-plt.show()
 
 # Get all the columns from the dataFrame
 columns = data.columns.tolist()
@@ -72,15 +55,13 @@ classifiers = {
     "Isolation Forest": IsolationForest(max_samples=len(X),
                                         contamination=outlier_fraction,
                                         random_state=state),
-    "Local Outlier Factor": LocalOutlierFactor(
-        n_neighbors=20,
-        contamination=outlier_fraction)}
-    
-plt.figure(figsize=(9, 7))
+    "Local Outlier Factor": LocalOutlierFactor(n_neighbors=20,
+                                               contamination=outlier_fraction)
+}
+
 n_outliers = len(Fraud)
 
-
-for i, (clf_name, clf) in enumerate(classifiers.items()):
+for clf_name, clf in classifiers.items():
     
     # fit the data and tag outliers
     if clf_name == "Local Outlier Factor":
@@ -98,6 +79,6 @@ for i, (clf_name, clf) in enumerate(classifiers.items()):
     n_errors = (y_pred != Y).sum()
 
     # Run classification metrics
-    print('{}: {}'.format(clf_name, n_errors))
-    print(accuracy_score(Y, y_pred))
+    print(f'{clf_name}: {n_errors} errors')
+    print(f'Accuracy: {accuracy_score(Y, y_pred)}')
     print(classification_report(Y, y_pred))
